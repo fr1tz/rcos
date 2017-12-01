c35c155c41693b1f10cb2b34673377be3fdca35d
Michael Goldener <mg@wasted.ch>
2017-12-01 23:22:13 +0100

    Trim rlib.gd copyright+license header.

--- rlib/rlib.gd
+++ rlib/rlib.gd
@@ -1,4 +1,3 @@
-#-------------------------------------------------------------------------------
 # Copyright © 2017 Michael Goldener <mg@wasted.ch>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,7 +17,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#-------------------------------------------------------------------------------
 
 extends Node
 
