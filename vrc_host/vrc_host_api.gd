1a4c3bd105be5b74bded2b0b8855c68b91d55e9c
Michael Goldener <mg@wasted.ch>
2018-01-04 21:02:48 +0100

    Update copyright notices for 2018.

--- vrc_host/vrc_host_api.gd
+++ vrc_host/vrc_host_api.gd
@@ -1,4 +1,4 @@
-# Copyright © 2017 Michael Goldener <mg@wasted.ch>
+# Copyright © 2017, 2018 Michael Goldener <mg@wasted.ch>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
