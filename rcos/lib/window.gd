32d410e3a042e2f9ac977ea5a6adfed339539f95
Michael Goldener <mg@wasted.ch>
2018-03-16 09:22:47 +0100

    RCOS window bugfix

--- rcos/lib/window.gd
+++ rcos/lib/window.gd
@@ -45,8 +45,8 @@ func _canvas_input(event):
 	var index = 0
 	if touchscreen:
 		index = event.index
-	event.pos = get_pos() + (event.pos - get_pos()).rotated(-get_rotation())
-	var win_rect = get_rect()
+	var win_rect = get_global_rect()
+	event.pos = win_rect.pos + (event.pos - win_rect.pos).rotated(-get_rotation())
 	if !win_rect.has_point(event.pos):
 		mCanvas.update_input(index, null, false)
 		return
