6314bc9ab636aa34784f0348c4d1225ff70a2b76
Michael Goldener <mg@wasted.ch>
2018-06-29 22:00:45 +0200

    RCOS Data Connector: Commit current development version.

--- rcos/data_connector/data_connector_gui.gd
+++ rcos/data_connector/data_connector_gui.gd
@@ -1,4 +1,4 @@
-# Copyright © 2017, 2018 Michael Goldener <mg@wasted.ch>
+# Copyright © 2018 Michael Goldener <mg@wasted.ch>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
