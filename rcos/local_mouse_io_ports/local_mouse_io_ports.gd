4be7a4f26aeeee03fc6e7bf9e6ba2146997127b7
Michael Goldener <mg@wasted.ch>
2018-07-08 20:33:16 +0200

    RCOS: Mouse IO Ports: Commit current development version.

--- rcos/local_mouse_io_ports/local_mouse_io_ports.gd
+++ rcos/local_mouse_io_ports/local_mouse_io_ports.gd
@@ -15,13 +15,18 @@
 
 extends Node
 
-const PORT_TYPE_POS = 0
-const PORT_TYPE_SPEED = 1
-const PORT_TYPE_BUTTON = 2
+const PORT_POS = 0
+const PORT_SPEED = 1
+const PORT_BUTTON = 2
+const PORT_CAPTURED = 3
 
 var mMouseSpeed = Vector2(0, 0)
 var mLastMouseMovementSpeed = Vector2(0, 0)
+
+var mOutputPortsMeta = {}
+var mInputPortsMeta = {}
 var mOutputPorts = []
+var mInputPorts = []
 
 func _ready():
 	_add_io_ports()
@@ -31,58 +36,66 @@ func _exit_tree():
 	_remove_io_ports()
 
 func _add_io_ports():
-	var root_prefix = "local/mouse"
-	# Pos
-	for port_name in ["x", "y", "xy"]:
-		var prefix = root_prefix + "/pos"
-		var port_path = prefix+"/"+port_name
-		var port = data_router.add_output_port(port_path)
-		port.set_meta("port_type", PORT_TYPE_POS)
-		port.connect("data_access", self, "_output_port_data_access", [port])
-		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
-		mOutputPorts.push_back(port)
-	var pos_node = data_router.get_output_port(root_prefix+"/pos")
-	pos_node.set_meta("icon32", load("res://data_router/icons/32/pointer.png"))
-	# Speed
-	for port_name in ["x", "y", "xy"]:
-		var prefix = root_prefix + "/speed"
-		var port_path = prefix+"/"+port_name
-		var port = data_router.add_output_port(port_path)
-		port.set_meta("port_type", PORT_TYPE_SPEED)
-		port.connect("data_access", self, "_output_port_data_access", [port])
-		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
-		mOutputPorts.push_back(port)
-	var speed_node = data_router.get_output_port(root_prefix+"/speed")
-	speed_node.set_meta("icon32", load("res://data_router/icons/32/speedometer.png"))
-	# Buttons
-	for button_index in range(1, 17):
-		var prefix = root_prefix + "/buttons/"+str(button_index)
-		var port_name = "pressed"
-		var port_path = prefix+"/"+port_name
-		var port = data_router.add_output_port(port_path)
-		port.set_meta("port_type", PORT_TYPE_BUTTON)
-		port.set_meta("button_index", button_index)
-		port.connect("data_access", self, "_output_port_data_access", [port])
-		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
-		port.get_parent().set_meta("icon32", load("res://data_router/icons/32/button.png"))
-		mOutputPorts.push_back(port)
-
-#	for axis_index in range(0, 8):
-#		var port_name = "axis/"+str(axis_index+1)
-#		var port_path = prefix+"/"+port_name
-#		var port = data_router.add_output_port(port_path)
-#		port.set_meta("port_type", PORT_TYPE_AXIS)
-#		port.set_meta("axis_index", axis_index)
-#		port.connect("data_accessed", self, "_output_port_data_accessed", [port])
-#		port.connect("connections_changed", self, "_output_port_connections_changed", [port])
-#		mOutputPorts.push_back(port)
+	var prefix = "local/mouse"
+	_add_output_ports(prefix)
+	_add_input_ports(prefix)
 
 func _remove_io_ports():
 	for port in mOutputPorts:
 		data_router.remove_port(port)
 
+func _add_output_ports(prefix):
+	# Pointer Pos
+	for port_name in ["x", "y", "xy"]:
+		var port = data_router.add_output_port(prefix+"/pos/"+port_name)
+		port.set_meta("data_type", "float")
+		port.set_meta("port_type", PORT_POS)
+		port.connect("data_access", self, "_output_port_data_access", [port])
+		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
+		mOutputPorts.push_back(port)
+	var pos_node = data_router.get_output_port(prefix+"/pos")
+	pos_node.set_meta("icon32", load("res://data_router/icons/32/pointer.png"))
+	# Mouse Speed
+	for port_name in ["x", "y", "xy"]:
+		var port = data_router.add_output_port(prefix+"/speed/"+port_name)
+		port.set_meta("data_type", "float")
+		port.set_meta("port_type", PORT_SPEED)
+		port.connect("data_access", self, "_output_port_data_access", [port])
+		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
+		mOutputPorts.push_back(port)
+	var speed_node = data_router.get_output_port(prefix+"/speed")
+	speed_node.set_meta("icon32", load("res://data_router/icons/32/speedometer.png"))
+	# Mouse Buttons
+	for button_index in range(1, 17):
+		var port = data_router.add_output_port(prefix+"/buttons/"+str(button_index))
+		port.set_meta("data_type", "bool")
+		port.set_meta("port_type", PORT_BUTTON)
+		port.set_meta("button_index", button_index)
+		port.connect("data_access", self, "_output_port_data_access", [port])
+		#port.connect("connections_changed", self, "_output_port_connections_changed", [port])
+		port.get_parent().set_meta("icon32", load("res://data_router/icons/32/button.png"))
+		mOutputPorts.push_back(port)
+	# Captured
+	var port = data_router.add_output_port(prefix+"/captured")
+	port.set_meta("data_type", "bool")
+	port.set_meta("port_type", PORT_CAPTURED)
+	port.connect("data_access", self, "_output_port_data_access", [port])
+
+func _add_input_ports(prefix):
+	mInputPortsMeta["captured"] = {
+		"port_class": PORT_CAPTURED,
+		"data_type": "bool"
+	}
+	for port_path in mInputPortsMeta.keys():
+		var port = data_router.add_input_port(prefix+"/"+port_path)
+		for meta_name in mInputPortsMeta[port_path].keys():
+			var meta_value = mInputPortsMeta[port_path][meta_name]
+			port.set_meta(meta_name, meta_value)
+		port.connect("data_changed", self, "_input_port_data_changed", [port])
+		mInputPorts.push_back(port)
+
 func _update_output_port_data(port):
-	if port.get_meta("port_type") == PORT_TYPE_POS:
+	if port.get_meta("port_type") == PORT_POS:
 		var new_data
 		var pos = get_viewport().get_mouse_pos()
 		if port.get_name() == "xy":
@@ -93,7 +106,7 @@ func _update_output_port_data(port):
 			new_data = pos.y
 		if port.mData != new_data:
 			port.put_data(new_data)
-	elif port.get_meta("port_type") == PORT_TYPE_SPEED:
+	elif port.get_meta("port_type") == PORT_SPEED:
 		var new_data
 		var speed = mMouseSpeed
 		if port.get_name() == "xy":
@@ -104,20 +117,31 @@ func _update_output_port_data(port):
 			new_data = speed.y
 		if port.mData != new_data:
 			port.put_data(new_data)
-	elif port.get_meta("port_type") == PORT_TYPE_BUTTON:
+	elif port.get_meta("port_type") == PORT_BUTTON:
 		var button_index = port.get_meta("button_index")
 		var val = Input.is_mouse_button_pressed(button_index)
 		if port.mData != val:
 			port.put_data(val)
+	elif port.get_meta("port_type") == PORT_CAPTURED:
+		var captured = false
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			captured = true
+		if port.mData != captured:
+			port.put_data(captured)
 
 func _output_port_data_access(port):
 	_update_output_port_data(port)
 
-#func _output_port_connections_changed(port):
-#	set_fixed_process(false)
-#	for port in mOutputPorts:
-#		if port.is_connected():
-#			set_fixed_process(true)
+func _input_port_data_changed(old_data, new_data, port):
+	var port_class = port.get_meta("port_class")
+	if port_class == PORT_CAPTURED:
+		var captured = false
+		if new_data != null:
+			captured = bool(new_data)
+		if captured:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _fixed_process(delta):
 	var last_mouse_movement_speed = Input.get_mouse_speed()
