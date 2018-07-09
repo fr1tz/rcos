6cc32d1d6d11a8327bd5343567230d627eab8c0d
Michael Goldener <mg@wasted.ch>
2018-07-09 16:42:19 +0200

    RCOS WM: Add "dangling controls" functionality (used for drag'n'drop).

--- rcos/wm/wm.gd
+++ rcos/wm/wm.gd
@@ -22,6 +22,7 @@ onready var mWindow = get_node("desktop/window")
 onready var mTaskbar = get_node("taskbar")
 
 var mActiveTask = null
+var mDanglingControls = {}
 
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
@@ -62,6 +63,26 @@ func _resized():
 #		var dpi = OS.get_screen_dpi(screen_num)
 #		var root_canvas_size = screen_size * (120.0/dpi)
 #		root_canvas.resize(root_canvas_size)
+
+func _canvas_input(event):
+	var touchscreen = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.SCREEN_DRAG)
+	var touch = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.MOUSE_BUTTON)
+	var drag = (event.type == InputEvent.SCREEN_DRAG || event.type == InputEvent.MOUSE_MOTION)
+	if !touch && !drag:
+		return
+	var index = 0
+	if touchscreen:
+		index = event.index
+	if !mDanglingControls.has(index):
+		return
+	var dangling_control = mDanglingControls[index]
+	dangling_control.set_pos(event.pos - dangling_control.get_size()/2)
+	if touch && !event.pressed:
+		get_node("overlay/dangling_controls").remove_child(dangling_control)
+		dangling_control.queue_free()
+		mDanglingControls.erase(index)
+		if mDanglingControls.size() == 0:
+			rcos.disable_canvas_input(self)
 
 func _on_task_changed(task):
 	if task == mActiveTask:
@@ -130,3 +151,30 @@ func show_task(task_id):
 	mWindow.show_canvas(canvas, canvas_region)
 	mTaskbar.set_hidden(fullscreen)
 	mTaskbar.mark_active_task(task_id)
+
+func pick_up_control(control, index):
+	if control == null:
+		return
+	# Don't "pick up" the control if the screen isn't touched.
+	var pos = get_viewport().get_screen_touch_pos(index)
+	if pos == null:
+		control.queue_free()
+		return
+	if mDanglingControls.has(index):
+		var old_control = mDanglingControls[index]
+		get_node("overlay/dangling_controls").remove_child(old_control)
+		old_control.queue_free()
+		mDanglingControls.erase(index)
+	get_node("overlay/dangling_controls").add_child(control)
+	control.set_pos(pos - control.get_size()/2)
+	mDanglingControls[index] = control
+	if mDanglingControls.size() > 0:
+		rcos.enable_canvas_input(self)
+	else:
+		rcos.disable_canvas_input(self)
+
+func get_dangling_control(index):
+	if mDanglingControls.has(index):
+		return mDanglingControls[index]
+	else:
+		return null
