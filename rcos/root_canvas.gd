822da909352678d559b7c94ed2f4cb8fdc16b512
Michael Goldener <mg@wasted.ch>
2018-07-09 16:32:56 +0200

    RCOS: Root Canvas: Implement get_screen_touch_pos(index) method.

--- rcos/root_canvas.gd
+++ rcos/root_canvas.gd
@@ -18,7 +18,8 @@ extends Viewport
 const DEFAULT_TARGET_FPS = 30
 const MAX_TARGET_FPS = 120
 
-var mScreenTouches = 0
+var mScreenTouches = {}
+var mNumScreenTouches = 0
 var mLastInputEventId = 0
 
 func _ready():
@@ -27,18 +28,28 @@ func _ready():
 
 func _input(event):
 	mLastInputEventId = event.ID
+	var touchscreen = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.SCREEN_DRAG)
 	var touch = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.MOUSE_BUTTON)
+	var drag = (event.type == InputEvent.SCREEN_DRAG || event.type == InputEvent.MOUSE_MOTION)
+	var index = 0
+	if touchscreen:
+		index = event.index
 	var from_user = event.device >= 0
-	if from_user && touch:
-		if event.pressed:
-			mScreenTouches += 1
-		else:
-			mScreenTouches -= 1
-		#prints("root_canvas:", event, "->", mScreenTouches)
-		if mScreenTouches > 0:
-			OS.set_target_fps(MAX_TARGET_FPS)
-		else:
-			OS.set_target_fps(DEFAULT_TARGET_FPS)
+	if from_user:
+		if drag:
+			mScreenTouches[index] = event.pos
+		elif touch:
+			if event.pressed:
+				mScreenTouches[index] = event.pos
+				mNumScreenTouches += 1
+			else:
+				mScreenTouches.erase(index)
+				mNumScreenTouches -= 1
+			#prints("root_canvas:", event, "->", mNumScreenTouches)
+			if mNumScreenTouches > 0:
+				OS.set_target_fps(MAX_TARGET_FPS)
+			else:
+				OS.set_target_fps(DEFAULT_TARGET_FPS)
 	var group = "_canvas_input"+str(get_instance_ID())
 	if get_tree().has_group(group):
 		get_tree().call_group(1|2|8, group, "_canvas_input", event)
@@ -48,3 +59,8 @@ func get_next_input_event_id():
 
 func is_displayed():
 	return true
+
+func get_screen_touch_pos(index):
+	if mScreenTouches.has(index):
+		return mScreenTouches[index]
+	return null
