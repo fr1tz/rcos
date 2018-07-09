77b7768c39444269c1d5319453890d9aceaefd0b
Michael Goldener <mg@wasted.ch>
2018-07-09 21:19:46 +0200

    Text Edit Widget: Commit current development version.

--- lsr_widgets/text_edit_widget/text_edit_widget.gd
+++ lsr_widgets/text_edit_widget/text_edit_widget.gd
@@ -15,28 +15,29 @@
 
 extends Node
 
-var mOutputPorts = {}
-var mInputPorts = {}
+var mOutputPorts = []
+var mInputPorts = []
 
-onready var mTextEditor = get_node("main_canvas/gui/frame/text_editor")
+onready var mTextEditor = get_node("main_canvas/main_gui/frame/text_editor")
 
 func _ready():
 	var port_path_prefix = "local/text_edit_widget"+str(get_meta("widget_id"))
-	mInputPorts["text"] = data_router.add_input_port(port_path_prefix+"/text")
-	mInputPorts["text"] = data_router.add_input_port(port_path_prefix+"/append(line)")
-	for port in mInputPorts.values():
+	mInputPorts.push_back(data_router.add_input_port(port_path_prefix+"/text"))
+	mInputPorts.push_back(data_router.add_input_port(port_path_prefix+"/append(line)"))
+	for port in mInputPorts:
 		port.connect("data_changed", self, "_on_input_port_data_changed", [port])
-	mOutputPorts["text"] = data_router.add_output_port(port_path_prefix+"/text")
+	mOutputPorts.push_back(data_router.add_output_port(port_path_prefix+"/text"))
 	mTextEditor.connect("text_changed", self, "_on_text_changed")
+	get_main_gui().connect("dangling_control_dropped", self, "_dangling_control_dropped")
 
 func _exit_tree():
-	for port in mOutputPorts.values():
+	for port in mOutputPorts:
 		data_router.remove_port(port)
-	for port in mInputPorts.values():
+	for port in mInputPorts:
 		data_router.remove_port(port)
 
 func _on_text_changed():
-	mOutputPorts["text"].put_data(mTextEditor.get_text())
+	mOutputPorts[0].put_data(mTextEditor.get_text())
 
 func _on_input_port_data_changed(old_data, new_data, port):
 	if port.get_name() == "text":
@@ -50,12 +51,21 @@ func _on_input_port_data_changed(old_data, new_data, port):
 			mTextEditor.set_text(mTextEditor.get_text()+line)
 			mTextEditor.cursor_set_line(mTextEditor.get_line_count(), true)
 
+func _dangling_control_dropped(control):
+	if !control.has_meta("data"):
+		return
+	var data = control.get_meta("data")
+	if data == null:
+		mTextEditor.set_text("")
+	else:
+		mTextEditor.set_text(str(data))
+
 #-------------------------------------------------------------------------------
 # Common Widget API
 #-------------------------------------------------------------------------------
 
 func get_main_gui():
-	return get_node("main_canvas/gui")
+	return get_node("main_canvas/main_gui")
 
 func get_config_gui():
 	return null
