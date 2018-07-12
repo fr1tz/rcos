f65cc0e40858730cf3ae68d3d66aecbe0e7bd11c
Michael Goldener <mg@wasted.ch>
2018-07-12 12:26:23 +0200

    Text Edit Widget: Commit current development version.

---
+++ lsr_widgets/text_edit_widget/main_gui.gd
@@ -0,0 +1,40 @@
+# Copyright © 2018 Michael Goldener <mg@wasted.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extends Panel
+
+var mWidgetHost = null
+
+func _init():
+	add_user_signal("dangling_control_dropped")
+
+func _ready():
+	mWidgetHost = get_meta("widget_host_api")
+	mWidgetHost.enable_canvas_input(self)
+
+func _canvas_input(event):
+	var touchscreen = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.SCREEN_DRAG)
+	var touch = (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.MOUSE_BUTTON)
+	var drag = (event.type == InputEvent.SCREEN_DRAG || event.type == InputEvent.MOUSE_MOTION)
+	if !touch && !drag:
+		return
+	var index = 0
+	if touchscreen:
+		index = event.index
+	if touch && !event.pressed:
+		var control = rcos.gui.get_dangling_control(index)
+		if control != null:
+			emit_signal("dangling_control_dropped", control)
+
