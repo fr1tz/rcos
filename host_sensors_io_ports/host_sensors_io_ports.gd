4ea0b469cc753139b404d44c5963ac1a81a52049
Michael Goldener <mg@wasted.ch>
2018-07-18 11:53:50 +0200

    Add 'host_sensors_io_ports' module: Provides access to host OS sensors via i/o ports.

---
+++ host_sensors_io_ports/host_sensors_io_ports.gd
@@ -0,0 +1,22 @@
+# Copyright © 2018 Michael Goldener <mg@wasted.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extends Node
+
+func _ready():
+	var os_name = OS.get_name().to_lower().replace(" ", "_")
+	var port_path_prefix = "localhost/"+os_name+"/sensors"
+	var node = data_router.get_output_port(port_path_prefix)
+	node.set_meta("icon32", load("res://data_router/icons/32/sensor.png"))
---
+++ host_sensors_io_ports/host_sensors_io_ports.m
@@ -0,0 +1,7 @@
+[module]
+
+
+desc="Provides access to host OS sensors via i/o ports"
+
+
+path="host_sensors_io_ports.tscn"
